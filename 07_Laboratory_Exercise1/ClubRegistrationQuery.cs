c06890b65145da0d4723f934aad7c5782e685db1
NavhiAngeles <ivhanangeles.04@gmail.com>
2025-11-19 20:05:50 +0800

    already insert the given code

--- 07_Laboratory_Exercise1/ClubRegistrationQuery.cs
+++ 07_Laboratory_Exercise1/ClubRegistrationQuery.cs
@@ -28,7 +28,49 @@ namespace _07_Laboratory_Exercise1
 
         public ClubRegistrationQuery()
         {
-               
+            connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ClubDBAngeles;Integrated Security=True;Encrypt=False;
+            TrustServerCertificate=True;userid=angeles.i;password=12345";
+            sqlConnect = new SqlConnection(connectionString);
+            sqlCommand = new SqlCommand();
+            /*sqlAdapter = new SqlDataAdapter();*/
+            dataTable = new DataTable();
+            bindingSource = new BindingSource();
         }
+
+        public bool DisplayList()
+        {
+            try
+            {
+                string ViewClubMembers ="SELECT StudentID, FirstName, MiddleName, LastName, Age, Gender, Program " + "FROM ClubMembers";
+                sqlAdapter = new SqlDataAdapter(ViewClubMembers, sqlConnect);
+                dataTable.Clear();
+                sqlAdapter.Fill(dataTable);
+                bindingSource.DataSource = dataTable;
+                return true; 
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool RegisterStudent(int ID, long StudentID, string FirstName, string MiddleName, string LastName, int Age, string Gender, string Program)
+        {
+            sqlCommand = new SqlCommand("INSERT INTO ClubMembers VALUES(@ID, @StudentID,@FirstName, @MiddleName, @LastName, @Age, @Gender, @Program)", sqlConnect);
+            sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            sqlCommand.Parameters.Add("@RegistrationID", SqlDbType.BigInt).Value = StudentID;
+            sqlCommand.Parameters.Add("@StudentID", SqlDbType.VarChar).Value = StudentID;
+            sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName;
+            sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName;
+            sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName;
+            sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age;
+            sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender;
+            sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+            sqlConnect.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnect.Close();
+            return true;
+        }
+
+
     }
 }
